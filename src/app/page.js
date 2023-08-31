5a8a4bc626b4a7797e7e5a3d53170fc38c096203
Baco CCB <Baco@ccb.edu.gt>
2023-08-31 08:48:38 -0600

    Add info section

--- src/app/page.js
+++ src/app/page.js
@@ -15,10 +15,45 @@ export default function Home() {
               <span>I am</span>
             </span>
             <br />
-            <span>Josh Carlson</span>
+            <span>André Flores</span>
           </h1>
         </div>
       </header>
+      <section>
+        <div>
+          <span>André</span>
+          <br />
+          <span>Flores</span>
+        </div>
+      </section>
+      <section className={styles.infoSection}>
+        <img
+        src='imagen-para-website.png'
+        alt='image'
+        className={styles.image}
+        />
+        <div className={styles.infoContainer}>
+          <span className={styles.title+" "+styles.yellowText}>André</span>
+          <br />
+          <span className={styles.title}>Flores</span>
+        </div>
+        <div className={styles.list}>
+          <ul>
+            <li>
+              <span className={styles.graytext}>Age: </span> 17
+            </li>
+            <li>
+              <span className={styles.graytext}>Nationality: </span> Guatemalan
+            </li>
+            <li>
+              <span className={styles.graytext}>Skill set: </span> Good chambing
+            </li>
+            <li>
+              <span className={styles.graytext}>Languages: </span> Spanish and English
+            </li>
+          </ul>
+        </div>
+      </section>
     </main>
 
   )
